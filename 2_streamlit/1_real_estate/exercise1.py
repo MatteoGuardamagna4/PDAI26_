a78d9d3de009956320fa6166da06fca3687721d9
MatteoGuardamagna4 <matteo.guardamagna@alumni.esade.edu>
2026-02-10 19:41:46 +0100

    done exercise 1 with selectbox, text input and the final write ( \n, """, \)

--- 2_streamlit/1_real_estate/exercise1.py
+++ 2_streamlit/1_real_estate/exercise1.py
@@ -7,15 +7,12 @@ st.header("Selec a property and get the information you need")
 st.write("Streamlit app to display real estate information in **Paris**")
 
 
-type_local = st.selectbox("Type local:", ['Dépendance', 'Appartement', 'Local industriel. commercial ou assimilé', 'Maison'])
 
 FILE = (
-    f"https://files.data.gouv.fr/geo-dvf/latest/csv/"
-    "departements/75/{type_local}.csv.gz"
+    f"https://files.data.gouv.fr/geo-dvf/latest/csv/2022/"
+    "departements/75.csv.gz"
 )
 df = pd.read_csv(FILE, compression="gzip", low_memory=False)
-
-
 
 street_name = st.text_input("Filter by street name", "")
 
@@ -23,4 +20,13 @@ if street_name:
     df.dropna(subset=["adresse_nom_voie"], inplace=True)
     df = df[df["adresse_nom_voie"].str.contains(street_name, case=False)]
 
+type_local = st.selectbox("type_local:", ['Dépendance', 'Appartement', 'Local industriel. commercial ou assimilé', 'Maison'])
+
+df = df[df['type_local'] == type_local]
+
 st.dataframe(df)
+
+st.write(f"""Number of places: {df.shape[0]} 
+         Average price: {df['valeur_fonciere'].mean(axis=0)}""")
+
+st.write(f"Number of places: {df.shape[0]} \nAverage price: {df['valeur_fonciere'].mean(axis=0)}")
